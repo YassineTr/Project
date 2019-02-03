43580beb750cf73e39d8d378ed8b99135053fe91
kwathek <w.kahla16035@pi.tn>
2019-02-03 02:15:48 +0100

    fix error & update bibliotheque

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxPopper } from 'angular-popper';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    NgxPopper,
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule,
     AppRoutingModule
   ],
   providers: [],
